8b822e0902ca449105848cb874e4f9a3ac606747
yux1004 <yux1004@gmail.com>
2018-04-07 19:57:48 -0400

    storage

--- main.go
+++ main.go
@@ -9,15 +9,20 @@ import (
   elastic "gopkg.in/olivere/elastic.v3"
   "github.com/pborman/uuid"
   "reflect"
+  // "cloud.google.com/go/bigtable"
   "context"
-  "cloud.google.com/go/bigtable"
-
+  "cloud.google.com/go/storage"
+  "io"
 )
 /*
 const (
 	DISTANCE = "200km"
 )
 */
+
+
+
+
 
 const (
       INDEX = "around"
@@ -30,7 +35,9 @@ const (
 
 
       // Needs to update this URL if you deploy it to cloud.
-      ES_URL = "http://35.184.61.148:9200" 
+      ES_URL = "http://35.184.171.234:9200" 
+
+      BUCKET_NAME = "post-images-ceremonial-tea-199302"
 )
 
 
@@ -44,6 +51,7 @@ type Post struct {
 	User     string `json:"user"`
 	Message  string  `json:"message"`
 	Location Location `json:"location"`
+  Url    string `json:"url"`
 }
 
 /*
@@ -106,6 +114,8 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Post received: %s\n", p.Message)
 }
 */
+
+/*
 func handlerPost(w http.ResponseWriter, r *http.Request) {
       // Parse from body of request to get a json object.
       fmt.Println("Received one post request")
@@ -148,6 +158,161 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 
 
 }
+*/
+
+
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+      // Other codes
+     w.Header().Set("Content-Type", "application/json")
+     w.Header().Set("Access-Control-Allow-Origin", "*")
+     w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+
+      // 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+      // After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
+      // If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
+      r.ParseMultipartForm(32 << 20)
+
+      // Parse from form data.
+      fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+      lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+      lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+      p := &Post{
+             User:    "1111",
+             Message: r.FormValue("message"),
+             Location: Location{
+                    Lat: lat,
+                    Lon: lon,
+             },
+      }
+
+      id := uuid.New()
+
+      file, _, err := r.FormFile("image")
+      if err != nil {
+             http.Error(w, "Image is not available", http.StatusInternalServerError)
+             fmt.Printf("Image is not available %v.\n", err)
+             return
+      }
+      defer file.Close()
+
+      ctx := context.Background()
+
+     // replace it with your real bucket name.
+      _, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+      if err != nil {
+             http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+             fmt.Printf("GCS is not setup %v\n", err)
+             return
+      }
+
+      // Update the media link after saving to GCS.
+      p.Url = attrs.MediaLink
+
+      // Save to ES.
+      saveToES(p, id)
+
+      // Save to BigTable.
+      //saveToBigTable(p, id)
+
+}
+// Save an image to GCS.
+func saveToGCS(ctx context.Context, r io.Reader, bucket, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+      client, err := storage.NewClient(ctx)
+      if err != nil {
+             return nil, nil, err
+      }
+      defer client.Close()
+
+      bh := client.Bucket(bucket)
+      // Next check if the bucket exists
+      if _, err = bh.Attrs(ctx); err != nil {
+             return nil, nil, err
+      }
+
+      obj := bh.Object(name)
+      w := obj.NewWriter(ctx)
+      if _, err := io.Copy(w, r); err != nil {
+             return nil, nil, err
+      }
+      if err := w.Close(); err != nil {
+             return nil, nil, err
+      }
+
+      
+      if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+             return nil, nil, err
+      }
+
+      attrs, err := obj.Attrs(ctx)
+      fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+      return obj, attrs, err
+}
+
+
+/*
+// after deleting big table
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+        // Parse from body of request to get a json object.
+        fmt.Println("Received one post request")
+        decoder := json.NewDecoder(r.Body)
+        var p Post
+        if err := decoder.Decode(&p); err != nil {
+                panic(err)
+                return
+        }
+
+        // Create a client
+        es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+        if err != nil {
+                panic(err)
+                return
+        }
+
+        id := uuid.New()
+
+        // Save it to index
+        _, err = es_client.Index().
+                Index(INDEX).
+                Type(TYPE).
+                Id(id).
+                BodyJson(p).
+                Refresh(true).
+                Do()
+        if err != nil {
+                panic(err)
+                return
+        }
+
+        fmt.Printf("Post is saved to Index: %s\n", p.Message)
+        /*
+        ctx := context.Background()
+        bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+        if err != nil {
+                panic(err)
+                return
+        }
+
+        tbl := bt_client.Open("post")
+        mut := bigtable.NewMutation()
+        t := bigtable.Now()
+        mut.Set("post", "user", t, []byte(p.User))
+        mut.Set("post", "message", t, []byte(p.Message))
+        mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+        mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+        err = tbl.Apply(ctx, id, mut)
+        if err != nil {
+                panic(err)
+                return
+        }
+        fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
+        //
+}
+*/
+
+
 
 // Save a post to ElasticSearch
 func saveToES(p *Post, id string) {
